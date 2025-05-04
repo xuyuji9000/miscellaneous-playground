60ba597e88d18fbef3be786598d78d31fd5d597d
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-04 19:15:43 +0800

    #61 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/01-hello-wrold.kts
    1. add the 'beginner-tour-content/' directory
    
    2. add  the hello-world learning example

---
+++ 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/01-hello-wrold.kts
@@ -0,0 +1,13 @@
+
+println("Hello, World!")
+
+// variables
+
+val popcorn     = 5
+val hotdog      = 7
+var customers   = 10
+
+// some customers leave the queue
+customers =8
+
+println(customers)
---
+++ 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/Readme.md
@@ -0,0 +1,7 @@
+Go through the [beginner tour contents](https://kotlinlang.org/docs/kotlin-tour-welcome.html).
+
+# Commands
+
+``` shell
+kotlin ./01-hello-wrold.kts
+```
