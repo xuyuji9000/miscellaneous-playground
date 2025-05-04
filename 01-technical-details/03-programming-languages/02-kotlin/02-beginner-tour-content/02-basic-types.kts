5e4a7d9db3973f9cf0bbd2b84131f3d6d589857d
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-04 19:50:47 +0800

    #61 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/02-basic-types.kts
    1. add a 'basic-types.kts' kotlin script
    
    2. add arithmetic examples

---
+++ 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/02-basic-types.kts
@@ -0,0 +1,13 @@
+var customer = 10
+
+// some customers leave the queue
+customers = 8
+
+customers  = customers + 3  // Example of addition : 11
+customers  += 7             // Example of addition : 18
+customers  -= 3             // Example of subtraction : 15
+customers  *= 2             // Example of multiplication : 30
+customers  /= 3             // Example of division : 10
+
+
+println(customers)
