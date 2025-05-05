4724c319e640673cbeec7cefdcbc8368f30ed645
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-05 09:22:31 +0800

    #61 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/04-control-flow.kts
    1. add 'when' examples

--- 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/04-control-flow.kts
+++ 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/04-control-flow.kts
@@ -23,3 +23,39 @@ println(if (a>b) a else b)
 // 2. When
 
 println("--- When ---")
+
+val obj1 = "Hello"
+
+when(obj1) {
+    
+    "1" -> println("one")
+    
+    "Hello" -> println("greeting")
+
+    else -> println("unknown")
+}
+
+// use when as an expression
+val obj2 = "Hello"
+
+val result = when(obj2) {
+    "1" -> "one"
+    "Hello" -> "greeting"
+    else -> "unknown"
+}
+
+println(result)
+
+// when can be used without a subject
+// to check a chain of boolean expressions
+val trafficLightState = "Red"
+
+val trafficAction = when {
+    
+    trafficLightState == "Green"  -> "Go"
+    trafficLightState == "Yellow" -> "Slow down"
+    trafficLightState == "Red"    -> "Stop"
+    else                           -> "Malfunction"
+}
+
+println(trafficAction)
