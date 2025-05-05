88b06b558f5c3f837bca0522e4703ca6b623cc9a
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-05 08:54:54 +0800

    #61 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/04-control-flow.kts
    1. add if condition, examples

---
+++ 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/04-control-flow.kts
@@ -0,0 +1,25 @@
+// 1. If 
+
+println("--- If ---")
+
+var d: Int
+
+val check = true
+
+if (check) {
+    d = 1
+} else {
+    d = 2
+}
+
+println(d)
+
+// use if as an expression
+val a = 1
+val b = 2
+
+println(if (a>b) a else b)
+
+// 2. When
+
+println("--- When ---")
