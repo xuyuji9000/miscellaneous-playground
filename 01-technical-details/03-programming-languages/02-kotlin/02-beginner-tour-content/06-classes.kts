1330bb114688b66010fe368e81980da8a5a88d47
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-05 15:58:58 +0800

    #61 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/06-classes.kts
    1. define member function in a class

--- 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/06-classes.kts
+++ 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/06-classes.kts
@@ -1,9 +1,18 @@
 class Contact(
-    val Id   : Int,
+    val id   : Int,
     var email: String
-)
+) {
+    fun printId() {
+        println(id)
+    }
+}
 
+// 1. Create instance
+println("--- Create instance ---")
 val contact = Contact(1, "mary@gmail.com")
+
+// 2. access property
+println("--- Access property ---")
 
 println(contact.email)
 
@@ -11,5 +20,8 @@ contact.email = "jane@gmail.com"
 
 println(contact.email)
 
+// 3. member functions
+println("--- Member functions ---")
+contact.printId()
 
 
