f9f23f22b85585255cf8a9781186824e0746148b
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-05 16:57:27 +0800

    #61 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/06-classes.kts
    1. add data class examples
    data class comes with additional member functions
    - print the instance
    - compare instances of a class
    - copy instances
    - etc

--- 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/06-classes.kts
+++ 01-technical-details/03-programming-languages/02-kotlin/02-beginner-tour-content/06-classes.kts
@@ -25,3 +25,46 @@ println("--- Member functions ---")
 contact.printId()
 
 
+// 4. data class
+println("--- Data class ---")
+
+data class User(
+    val name: String,
+    val id  : Int
+)
+
+// 4.1 print as string
+println("--- Print as string ---")
+val user = User("Alex", 1)
+
+// Automatically uses toString() function
+// So that output is easy to read
+println(user.toString())
+
+// 4.2 compare instances
+println("--- Compare instances ---")
+
+val user1 = User("Alex", 1)
+val user2 = User("Alex", 1)
+val user3 = User("Max", 2)
+
+println("user1 == user2: ${user1 == user2}") 
+println("user2 == user3: ${user2 == user3}") 
+
+// 4.3 copy instance
+println("--- Copy instance ---")
+val user4 = User("Alex", 1)
+
+
+println(user4.copy())
+println(user4.copy(name = "Max"))
+println(user4.copy(id = 3))
+
+
+
+
+
+
+
+
+
