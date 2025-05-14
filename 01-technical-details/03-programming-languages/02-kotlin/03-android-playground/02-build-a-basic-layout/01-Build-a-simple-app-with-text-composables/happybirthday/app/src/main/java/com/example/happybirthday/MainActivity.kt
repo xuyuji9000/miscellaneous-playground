9658ea62f5b2714992299e70dbe0504364580cf2
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-14 22:59:21 +0800

    #64 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/02-build-a-basic-layout/01-Build-a-simple-app-with-text-composables/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
    1. use column layout component
    to arrange two text components vertically

--- 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/02-build-a-basic-layout/01-Build-a-simple-app-with-text-composables/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/02-build-a-basic-layout/01-Build-a-simple-app-with-text-composables/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -36,17 +37,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
-        modifier = modifier
-    )
-    
-    Text(
-        text = from,
-        fontSize = 36.sp
-    )
+    Column(modifier = Modifier) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            modifier = modifier
+        )
+
+        Text(
+            text = from,
+            fontSize = 36.sp
+        )
+
+    }
+
 }
 
 @Preview(showBackground = true)
