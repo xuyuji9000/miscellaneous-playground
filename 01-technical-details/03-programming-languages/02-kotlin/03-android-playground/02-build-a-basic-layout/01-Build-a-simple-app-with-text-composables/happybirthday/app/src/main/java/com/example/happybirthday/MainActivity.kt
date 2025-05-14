8bc937d6c158beca60607a6964510b41a325b845
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-14 08:00:21 +0800

    #64 01-Build-a-simple-app-with-text-composables/happybirthday/
    1. adjust fontsize, lineHeight of text composable component

--- 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/02-build-a-basic-layout/01-Build-a-simple-app-with-text-composables/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/02-build-a-basic-layout/01-Build-a-simple-app-with-text-composables/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,6 +37,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(message: String, modifier: Modifier = Modifier) {
     Text(
         text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp,
         modifier = modifier
     )
 }
