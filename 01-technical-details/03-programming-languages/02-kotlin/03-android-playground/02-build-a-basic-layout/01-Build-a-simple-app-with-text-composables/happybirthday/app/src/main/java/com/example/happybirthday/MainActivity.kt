b859f2e91a626ce65efdc2e3f7b5f0c590b10691
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-16 08:27:17 +0800

    #64 01-Build-a-simple-app-with-text-composables/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
    1. in the greeting text component, align text to the center.
    
    2. for from text component, align it to the bottom end of the colloum row
    since, by custom signature should be at the bottom, right of a birthday card

--- 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/02-build-a-basic-layout/01-Build-a-simple-app-with-text-composables/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/02-build-a-basic-layout/01-Build-a-simple-app-with-text-composables/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,9 +12,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,17 +41,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp)
+    ) {
         Text(
             text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp,
-            modifier = modifier
+            textAlign = TextAlign.Center
+
         )
 
         Text(
             text = from,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
         )
 
     }
