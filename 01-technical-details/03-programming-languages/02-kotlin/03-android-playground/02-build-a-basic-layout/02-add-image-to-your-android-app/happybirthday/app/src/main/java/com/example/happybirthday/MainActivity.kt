72ae286171f4c6e297bc200a3daf8067d2a7b9bd
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-16 19:49:33 +0800

    #64 ./03-android-playground/02-build-a-basic-layout/02-add-image-to-your-android-app/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
    1. display image composable

--- 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/02-build-a-basic-layout/02-add-image-to-your-android-app/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/02-build-a-basic-layout/02-add-image-to-your-android-app/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -40,7 +41,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+fun GreetingText(
+    message: String,
+    from: String,
+    modifier: Modifier = Modifier
+) {
     Column(
         verticalArrangement = Arrangement.Center,
         modifier = modifier.padding(8.dp)
@@ -65,10 +70,28 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
 
 }
 
+@Composable
+fun GreetingImage(
+    message: String,
+    from: String,
+    modifier: Modifier= Modifier) {
+
+    val image = painterResource(R.drawable.androidparty)
+
+    Image(
+        painter = image,
+        contentDescription = null
+    )
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText("Happy Birthday Karl!", from = "from Emma")
+        GreetingImage(
+            message ="Happy Birthday Karl!",
+            from = "from Emma"
+        )
     }
 }
