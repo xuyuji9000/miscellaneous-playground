815e7bf5119fca4a61fe524cebb5ae7bd7c332c5
Karl Xu <karl.yogi.xu@gmai.com>
2025-05-17 22:14:54 +0800

    #64 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/03-add-image-to-your-android-app/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
    1. use the contentScale property of Image component.
    it scale the width or height of the image to the same or larger than the screen size
    
    2. alpha property of the Image component
    it can be set between 0.0F to 1.0F.
    it is used to control the opacity of the image

--- 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/03-add-image-to-your-android-app/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ 01-technical-details/03-programming-languages/02-kotlin/03-android-playground/03-add-image-to-your-android-app/happybirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -82,7 +83,9 @@ fun GreetingImage(
     Box(modifier){
         Image(
             painter = image,
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
         )
     }
 
