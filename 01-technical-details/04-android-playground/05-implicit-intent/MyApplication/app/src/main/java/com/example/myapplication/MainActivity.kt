48b15a2346575e709282cf6f257953aa521bc3e9
Karl Xu <karl.yogi.xu@gmai.com>
2025-08-16 17:55:56 +0800

    #71 01-technical-details/04-android-playground/05-implicit-intent
    1. add an implicit intent
    which intends to open a url

---
+++ 01-technical-details/04-android-playground/05-implicit-intent/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Column {
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                        Button(
+                            onClick = {
+                                // Implicit intent to open a web page
+                                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"))
+                                startActivity(intent)
+                            }
+                        ) {
+                            Text("Click Me!")
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
