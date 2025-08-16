692079056b48bdf44442dc27df3c0af4d39990d9
Karl Xu <karl.yogi.xu@gmai.com>
2025-08-16 17:19:55 +0800

    #70 01-technical-details/04-android-playground/04-jump-to-another-activity/helloworld1
    1. add helloworld1/ project
    for the 'invoke activity in another apk' experiment

---
+++ 01-technical-details/04-android-playground/04-jump-to-another-activity/helloworld1/app/src/main/java/com/example/helloworld1/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.helloworld1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloworld1.ui.theme.Helloworld1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Helloworld1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "World1",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Helloworld1Theme {
+        Greeting("Android")
+    }
+}
